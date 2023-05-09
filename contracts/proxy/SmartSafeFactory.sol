9fe26023ab16ce36aa1eaf12e747046c146571c8
Ricardo Passos <ricardo.passos.cr@gmail.com>
2023-05-09 17:57:09 -0300

    feat: smartsafe factory created

---
+++ contracts/proxy/SmartSafeFactory.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {SmartSafe} from "../core/SmartSafe.sol";
+
+contract SmartSafeFactory {
+    uint64 internal nonce = 0;
+
+    error InvalidContractAddress();
+    event Deployed(address indexed);
+
+    function computeSalt(address _owner) private view returns (bytes32) {
+        return keccak256(abi.encode(nonce, _owner));
+    }
+
+    function computeAddress(address[] calldata _owners, uint8 _threshold)
+        public
+        view
+        returns (address)
+    {
+        bytes32 salt = computeSalt(_owners[0]);
+
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(
+                            abi.encodePacked(
+                                bytes1(0xff),
+                                address(this),
+                                salt,
+                                keccak256(
+                                    abi.encodePacked(
+                                        type(SmartSafe).creationCode,
+                                        abi.encode(_owners, _threshold)
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function deploySmartSafe(address[] calldata _owners, uint8 _threshold)
+        external
+    {
+        bytes32 salt = computeSalt(_owners[0]);
+        address predictedAddress = computeAddress(_owners, _threshold);
+
+        SmartSafe newlyDeployedContract = new SmartSafe{salt: salt}(
+            _owners,
+            _threshold
+        );
+
+        if (address(newlyDeployedContract) != predictedAddress) {
+            revert InvalidContractAddress();
+        }
+
+        nonce++;
+
+        emit Deployed(address(newlyDeployedContract));
+    }
+}
